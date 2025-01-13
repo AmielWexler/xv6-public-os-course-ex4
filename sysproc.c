2ea05d9011134dc1a53e8d873914ccdbd3d81299
AmielWexler <97599576+AmielWexler@users.noreply.github.com>
2025-01-13 14:08:53 +0200

    Update sysproc.c

--- sysproc.c
+++ sysproc.c
@@ -6,6 +6,7 @@
 #include "memlayout.h"
 #include "mmu.h"
 #include "proc.h"
+#include "processInfo.h"
 
 int
 sys_fork(void)
@@ -88,4 +89,59 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+
+int
+sys_getNumProc(void)
+{
+    int count = 0;
+    struct proc *p;
+    acquire(&ptable.lock);
+    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+        if (p->state != UNUSED)
+            count++;
+    }
+    release(&ptable.lock);
+    return count;
+}
+
+int
+sys_getMaxPid(void)
+{
+    int max_pid = 0;
+    struct proc *p;
+    acquire(&ptable.lock);
+    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+        if (p->state != UNUSED && p->pid > max_pid)
+            max_pid = p->pid;
+    }
+    release(&ptable.lock);
+    return max_pid;
+}
+
+int
+sys_getProcInfo(void)
+{
+    int pid;
+    struct processInfo *pinfo;
+
+    if (argint(0, &pid) < 0 || argptr(1, (char **)&pinfo, sizeof(*pinfo)) < 0)
+        return -1;
+
+    struct proc *p;
+    acquire(&ptable.lock);
+    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++) {
+        if (p->pid == pid) {
+            pinfo->state = p->state;
+            pinfo->ppid = p->parent ? p->parent->pid : 0;
+            pinfo->sz = p->sz;
+            pinfo->nfd = 0; // Calculate number of open FDs (iterate over p->ofile[])
+            pinfo->nrswitch = p->ncs; // Update p->ncs as part of scheduler logic
+            release(&ptable.lock);
+            return 0;
+        }
+    }
+    release(&ptable.lock);
+    return -1; // PID not found
 }
